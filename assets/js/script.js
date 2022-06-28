2c80c17485f929801464f3fa9723d20d961c18dd
wanjirugachunji <wanjirugachunji@gmail.com>
2022-06-28 19:50:38 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ function handleclick(){
     alert("Thankyou for visiting us")
 }
 
-var image = document.getElementsByClassName("baseimage");
-image.addEventlistener("mouseover", function(){
-    this.style ="box-shadow: 2px 2px 2px grey"
+// var image = document.getElementsByClassName("baseimage");
+// image.addEventlistener("mouseover", function(){
+//     this.style ="box-shadow: 2px 2px 2px grey"
+// });
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("features").addEventListener("click")(style.display==='none')
+    
 });
